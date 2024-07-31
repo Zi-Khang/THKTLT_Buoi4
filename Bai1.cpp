a33b29816b411accfb003ce4b64bcd72aceb8486
Vi Khang <trieuvikhang003@gmail.com>
2024-07-31 16:57:09 +0700

    Bai 1.1 va 1.5

--- Bai1.cpp
+++ Bai1.cpp
@@ -6,16 +6,24 @@ int giaiThua(int n)
 {
 	if (n == 1)
 		return 1;
-	return n * giaiThua(n - 1);
+	return n + giaiThua(n - 1);
 }
+
+int giaiThua1(int n)
+{
+	if (n == 1)
+		return n*(n+1);
+	return n * (n + 1) + giaiThua1(n - 1);
+}	
 
 int main()
 {
 	int n;
 	printf("Hay nhap so n: ");
 	scanf("%d", &n);
-
-	printf("Giai thua %d la: %d", n, giaiThua(n));
-	
+	printf("1 + 2 + 3 + ... + n: \n");
+	printf("Ket qua: %d\n", giaiThua(n));
+	printf("1.2 + 2.3 + 3.4 + ... + n.(n+1): ");
+	printf("Ket qua: %d\n ", giaiThua1(n));
 	getch();
 }
